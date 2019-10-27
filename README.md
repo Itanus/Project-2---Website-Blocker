88f18124503043c488933a447983b288a53dd1d2
Itanus <rob.cwi@hotmail.com>
2019-10-27 20:50:24 +0100

    changed file name

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Project-2---Website-Blocker
+App that blocks access to certain (www.amazon.com) sites in certain periods of time by modifying hosts file.
--- web_blocker.py
+++ web_blocker.py
@@ -1,3 +1,30 @@
-host_path = r"C:\Windows\System32\drivers\etc\hosts"
+import time
+from datetime import datetime as dt
+
+
+host_path = r"C:\Windows\System32\drivers\etc\hosts"                #directory in which needed file is located
 redirect = "127.0.0.1"
-websites = ["www.amazon.com", "amazon.com"]
+websites = ["www.amazon.com", "amazon.com"]                         #list of websites that are going to be blocked
+hour_start = input("Starting hour of the blockade?:\n")
+hour_end = input("Ending hour of the blockade?:\n")
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day, int(hour_start)) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, int(hour_end)):
+        print("Blocking\n")
+        with open(host_path, "r+") as file:
+            content = file.read()
+            for website in websites:
+                if website in  content:
+                    pass
+                else:
+                    file.write(redirect + " " + website + "\n")
+    else:
+        print("Blockade lifted\n")
+        with open(host_path, "r+") as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in websites):
+                    file.write(line)
+            file.truncate()
+    time.sleep(5)
